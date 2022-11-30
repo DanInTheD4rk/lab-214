548975910eb2509b752bd414c969080825767c92
daninthed4rk <daninthed4rk@gmail.com>
2022-11-30 10:37:46 -0500

    fix: mobile scroll issue

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import bg from "../public/bgLogoless.png"
 const Layout = ({ children }) => {
 	return (
 		<div
-			className="fixed bg-no-repeat bg-center bg-cover min-w-full min-h-screen"
+			className="absolute bg-no-repeat bg-center bg-cover min-w-full min-h-screen overflow-auto"
 			style={{ backgroundImage: `url(${bg.src})` }}
 		>
 			<NavBar />
