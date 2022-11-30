ced48baadebf8b50d2c79d5106915c5c9e19cb18
daninthed4rk <daninthed4rk@gmail.com>
2022-11-30 10:33:13 -0500

    mobile nav close on out click

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import bg from "../public/bgLogoless.png"
 const Layout = ({ children }) => {
 	return (
 		<div
-			className="absolute bg-no-repeat bg-center bg-cover min-w-full min-h-full overflow:clip"
+			className="fixed bg-no-repeat bg-center bg-cover min-w-full min-h-screen"
 			style={{ backgroundImage: `url(${bg.src})` }}
 		>
 			<NavBar />
--- components/NavBar.js
+++ components/NavBar.js
@@ -26,7 +26,7 @@ function classNames(...classes) {
 	return classes.filter(Boolean).join(" ")
 }
 
-export default function Example() {
+export default function NavBar() {
 	const { loading } = useLoading()
 	return (
 		<>
@@ -36,9 +36,9 @@ export default function Example() {
 					<>
 						<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 							<div className="relative flex h-16 items-center justify-between">
+								{/* Mobile menu button*/}
 								<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-									{/* Mobile menu button*/}
-									<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+									<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
 										<span className="sr-only">Open main menu</span>
 										{open ? (
 											<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -47,10 +47,11 @@ export default function Example() {
 										)}
 									</Disclosure.Button>
 								</div>
+
 								<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 									<div className="flex flex-shrink-0 items-center">
-										<img className="block h-8 w-auto lg:hidden" src={logo.src} alt="Your Company" />
-										<img className="hidden h-8 w-auto lg:block" src={logo.src} alt="Your Company" />
+										<img className="block h-8 w-auto lg:hidden" src={logo.src} alt="Lab214" />
+										<img className="hidden h-8 w-auto lg:block" src={logo.src} alt="Lab214" />
 									</div>
 									<div className="hidden sm:ml-6 sm:block">
 										<div className="flex space-x-4">
@@ -133,54 +134,60 @@ export default function Example() {
 							</div>
 						</div>
 
-						<Disclosure.Panel className="sm:hidden bg-slate-900 h-80">
-							<Disclosure>
-								<Disclosure.Button className="pt-2 px-5 font-bold text-white flex flex-row items-center justify-center">
-									Labs
-									<ChevronDownIcon
-										className={classNames(
-											open ? "text-gray-300" : "text-gray-300",
-											"mt-1 ml-2 h-5 w-5 group-hover:text-gray-700"
-										)}
-										aria-hidden="true"
-									/>
-								</Disclosure.Button>
-								<Disclosure.Panel className="text-gray-500">
-									<div className="overflow-hidden m-4 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-										<div className="relative grid gap-6 bg-slate-800 px-5 py-6 sm:gap-8 sm:p-8">
-											{labs.map((lab) => (
-												<a
-													key={lab.name}
-													href={lab.href}
-													className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-700"
-												>
-													<div className="ml-4">
-														<p className="text-base font-medium text-white">{lab.name}</p>
-														<p className="mt-1 text-sm text-gray-300">{lab.description}</p>
-													</div>
-												</a>
-											))}
-										</div>
+						{/* Mobile */}
+						{open ? (
+							<div className="h-screen">
+								<Disclosure.Panel className="sm:hidden bg-slate-900 min-h-[30%]">
+									<Disclosure>
+										<Disclosure.Button className="pt-2 px-5 font-bold text-white flex flex-row items-center justify-center">
+											Labs
+											<ChevronDownIcon
+												className={classNames(
+													open ? "text-gray-300" : "text-gray-300",
+													"mt-1 ml-2 h-5 w-5 group-hover:text-gray-700"
+												)}
+												aria-hidden="true"
+											/>
+										</Disclosure.Button>
+										<Disclosure.Panel className="text-gray-500">
+											<div className="overflow-hidden m-4 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+												<div className="relative grid gap-6 bg-slate-800 px-5 py-6 sm:gap-8 sm:p-8">
+													{labs.map((lab) => (
+														<a
+															key={lab.name}
+															href={lab.href}
+															className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-700"
+														>
+															<div className="ml-4">
+																<p className="text-base font-medium text-white">{lab.name}</p>
+																<p className="mt-1 text-sm text-gray-300">{lab.description}</p>
+															</div>
+														</a>
+													))}
+												</div>
+											</div>
+										</Disclosure.Panel>
+									</Disclosure>
+									<div className="space-y-1 px-2 pt-2 pb-3">
+										{navigation.map((item) => (
+											<Disclosure.Button
+												key={item.name}
+												as="a"
+												href={item.href}
+												className={classNames(
+													item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+													"block px-3 py-2 rounded-md text-base font-medium"
+												)}
+												aria-current={item.current ? "page" : undefined}
+											>
+												{item.name}
+											</Disclosure.Button>
+										))}
 									</div>
 								</Disclosure.Panel>
-							</Disclosure>
-							<div className="space-y-1 px-2 pt-2 pb-3">
-								{navigation.map((item) => (
-									<Disclosure.Button
-										key={item.name}
-										as="a"
-										href={item.href}
-										className={classNames(
-											item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
-											"block px-3 py-2 rounded-md text-base font-medium"
-										)}
-										aria-current={item.current ? "page" : undefined}
-									>
-										{item.name}
-									</Disclosure.Button>
-								))}
+								<Disclosure.Button className="sm:hidden h-full w-full cursor-default"></Disclosure.Button>
 							</div>
-						</Disclosure.Panel>
+						) : null}
 					</>
 				)}
 			</Disclosure>
