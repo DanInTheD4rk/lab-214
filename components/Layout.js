d6e809aa7d898a38c9cf8cdb68113b54d675287e
daninthed4rk <daninthed4rk@gmail.com>
2022-11-30 12:42:30 -0500

    fix: lab popover, home scroll, med screen no nav

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import bg from "../public/bgLogoless.png"
 const Layout = ({ children }) => {
 	return (
 		<div
-			className="absolute bg-no-repeat bg-center bg-cover min-w-full min-h-screen overflow-auto"
+			className="absolute bg-no-repeat bg-center bg-cover min-w-full min-h-screen overflow-hidden"
 			style={{ backgroundImage: `url(${bg.src})` }}
 		>
 			<NavBar />
--- components/NavBar.js
+++ components/NavBar.js
@@ -37,7 +37,7 @@ export default function NavBar() {
 						<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 							<div className="relative flex h-16 items-center justify-between">
 								{/* Mobile menu button*/}
-								<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+								<div className="absolute inset-y-0 left-0 flex items-center md:hidden">
 									<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
 										<span className="sr-only">Open main menu</span>
 										{open ? (
@@ -48,7 +48,7 @@ export default function NavBar() {
 									</Disclosure.Button>
 								</div>
 
-								<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+								<div className="flex flex-1 items-center justify-center sm:items-stretch md:justify-start">
 									<div className="flex flex-shrink-0 items-center">
 										<img className="block h-8 w-auto lg:hidden" src={logo.src} alt="Lab214" />
 										<img className="hidden h-8 w-auto lg:block" src={logo.src} alt="Lab214" />
@@ -86,7 +86,7 @@ export default function NavBar() {
 																leaveFrom="opacity-100 translate-y-0"
 																leaveTo="opacity-0 translate-y-1"
 															>
-																<Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
+																<Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0  ">
 																	<div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
 																		<div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
 																			{labs.map((lab) => (
@@ -137,7 +137,7 @@ export default function NavBar() {
 						{/* Mobile */}
 						{open ? (
 							<div className="h-screen">
-								<Disclosure.Panel className="sm:hidden bg-slate-900 min-h-[30%]">
+								<Disclosure.Panel className="md:hidden bg-slate-900 min-h-[30%]">
 									<Disclosure>
 										<Disclosure.Button className="pt-2 px-5 font-bold text-white flex flex-row items-center justify-center">
 											Labs
@@ -185,7 +185,7 @@ export default function NavBar() {
 										))}
 									</div>
 								</Disclosure.Panel>
-								<Disclosure.Button className="sm:hidden h-full w-full cursor-default"></Disclosure.Button>
+								<Disclosure.Button className="md:hidden h-full w-full cursor-default"></Disclosure.Button>
 							</div>
 						) : null}
 					</>
