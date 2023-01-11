cb3dcd487723ac7c5ad49b0e2365ff0b563c8a0a
daninthed4rk <daninthed4rk@gmail.com>
2023-01-11 16:19:53 -0500

    community update, cleanup

--- components/locator/Communities.js
+++ components/locator/Communities.js
@@ -1,6 +1,7 @@
 import kkServer from "../../public/kkServer.png"
 import discordLogo from "../../public/discordLogo.png"
 import twitterLogo from "../../public/twitterLogo.png"
+import telegramLogo from "../../public/telegramLogo.png"
 import { COMMUNITIES } from "../../constants/communities"
 
 const Communities = () => {
@@ -8,13 +9,13 @@ const Communities = () => {
 		<div className="text-white flex flex-row justify-center whitespace-nowrap">
 			<div>
 				{COMMUNITIES.map((com) => (
-					<div className="mx-4 my-2">{com.location}:</div>
+					<div className="text-sm ml-2 mr-2 my-2">{com.location}:</div>
 				))}
 			</div>
 
-			<div>
+			<div className="mt-1">
 				{COMMUNITIES.map((com) => (
-					<div className="mx-2 my-2">
+					<div className="mr-2 my-[4px]">
 						{com.kaijuChannel ? (
 							<a href={com.kaijuChannel}>
 								<img className="h-6 rounded-full" src={kkServer.src} />
@@ -26,9 +27,9 @@ const Communities = () => {
 				))}
 			</div>
 
-			<div>
+			<div className="mt-1">
 				{COMMUNITIES.map((com) => (
-					<div className="mx-2 my-2">
+					<div className="mr-2 my-[4px]">
 						{com.twitter ? (
 							<a href={com.twitter}>
 								<img className="h-6 rounded-full" src={twitterLogo.src} />
@@ -40,12 +41,26 @@ const Communities = () => {
 				))}
 			</div>
 
-			<div className="items-center">
+			<div className="items-center mt-1">
 				{COMMUNITIES.map((com) => (
-					<div className="mx-2 my-2">
+					<div className="mr-2 my-[4px]">
 						{com.discord ? (
 							<a href={com.discord}>
 								<img className="h-6 rounded-full" src={discordLogo.src} />
+							</a>
+						) : (
+							<br />
+						)}
+					</div>
+				))}
+			</div>
+
+			<div className="items-center mt-1">
+				{COMMUNITIES.map((com) => (
+					<div className="my-[4px]">
+						{com.telegram ? (
+							<a href={com.telegram}>
+								<img className="h-6 rounded-full" src={telegramLogo.src} />
 							</a>
 						) : (
 							<br />
--- pages/labs/locator.js
+++ pages/labs/locator.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState, useCallback, useMemo } from "react"
 import "mapbox-gl/dist/mapbox-gl.css"
 import mapboxgl from "!mapbox-gl"
 import Profile from "../../components/appInfo/Profile"
-import AppInfo from "../../components/appInfo/AppInfo"
 import Map, { Marker, Popup } from "react-map-gl"
 import marker from "../../public/marker.png"
 import { signIn, useSession } from "next-auth/react"
 import discordLogo from "../../public/discordLogo.png"
 import Communities from "../../components/locator/Communities"
+import TabBase from "../../components/TabBase"
 
 const styles = {
 	button:
@@ -18,12 +18,35 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX
 const contributors = [<Profile user="DanInTheD4rk" />]
 
 const locator = () => {
+	const tabs = [
+		{
+			title: "Map",
+			content: <MapTab />,
+		},
+		{
+			title: "Communities",
+			content: <Communities />,
+		},
+	]
+
+	const appInfo = {
+		contributors: contributors,
+		projectId: 8,
+	}
+
+	return (
+		<>
+			<TabBase appInfo={appInfo} tabs={tabs} />
+		</>
+	)
+}
+
+const MapTab = () => {
 	const { data: session } = useSession()
 	const [coordinates, setCoordinates] = useState({ longitude: 139.8, latitude: 35.68 })
 	const [locations, setLocations] = useState([])
 	const [hasUserLocation, setHasUserLocation] = useState(false)
 	const [popupInfo, setPopupInfo] = useState()
-	const [openTab, setOpenTab] = useState(1)
 
 	const markers = useMemo(
 		() =>
@@ -99,127 +122,70 @@ const locator = () => {
 	}, [])
 
 	return (
-		<>
-			<div className="mx-10 my-20">
-				<AppInfo contributors={contributors} projectId={8} />
-
-				<div className="flex flex-wrap">
-					<div className="w-full">
-						<ul className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row" role="tablist">
-							<li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-								<a
-									className={
-										"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-										(openTab === 1 ? "text-black bg-white bg-opacity-80" : "text-white bg-gray-800 bg-opacity-60")
-									}
-									onClick={(e) => {
-										e.preventDefault()
-										setOpenTab(1)
-									}}
-									data-toggle="tab"
-									href="#link1"
-									role="tablist"
-								>
-									<i className="fas fa-space-shuttle text-base mr-1"></i> Map
-								</a>
-							</li>
-							<li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-								<a
-									className={
-										"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-										(openTab === 2 ? "text-black bg-white bg-opacity-80" : "text-white bg-gray-800 bg-opacity-60")
-									}
-									onClick={(e) => {
-										e.preventDefault()
-										setOpenTab(2)
-									}}
-									data-toggle="tab"
-									href="#link2"
-									role="tablist"
-								>
-									<i className="fas fa-cog text-base mr-1"></i> Communities
-								</a>
-							</li>
-						</ul>
-						<div className="unset flex flex-col min-w-0 break-words bg-gray-800 w-full mb-6 shadow-lg rounded bg-opacity-60">
-							<div className="px-4 py-5 flex-auto">
-								<div className="tab-content tab-space">
-									<div className={openTab === 1 ? "block" : "hidden"} id="link1">
-										<div className="flex flex-wrap mt-2">
-											{!session && (
-												<div className="flex flex-row w-full flex-wrap justify-center text-xl font-bold bg-white p-3 bg-opacity-20 rounded-lg">
-													<button
-														type="button"
-														className={`m-10 mx-[20%] ${styles.button}`}
-														onClick={() => signIn("discord", { callbackUrl: "", redirect: true }, { prompt: "none" })}
-													>
-														Log In
-													</button>
-												</div>
-											)}
-											{session && (
-												<button
-													type="button"
-													className={`my-5 mx-10 ${styles.button}`}
-													onClick={hasUserLocation ? () => removeLocation() : () => saveLocation()}
-												>
-													{hasUserLocation ? "Remove Location" : "Save Location"}
-												</button>
-											)}
-											{session && (
-												<div className="w-full min-h-[600px]">
-													<Map
-														reuseMaps
-														{...coordinates}
-														initialViewState={{
-															longitude: 139.8,
-															latitude: 35.68,
-															zoom: 2,
-															minZoom: 2,
-															maxZoom: 15,
-														}}
-														onMove={(evt) => setCoordinates(evt.viewState)}
-														mapStyle="mapbox://styles/mapbox/streets-v12"
-													>
-														{!hasUserLocation && (
-															<Marker
-																longitude={coordinates.longitude}
-																latitude={coordinates.latitude}
-																draggable={true}
-																onDragEnd={onMarkerDragEnd}
-															>
-																<img className="max-h-[24px]" src={marker.src} />
-															</Marker>
-														)}
-														{markers}
-														{popupInfo && (
-															<Popup
-																anchor="bottom"
-																longitude={popupInfo.longitude}
-																latitude={popupInfo.latitude}
-																onClose={() => setPopupInfo(null)}
-															>
-																<div className="flex mx-2 mt-2">
-																	<img className="max-h-[20px] mr-2" src={discordLogo.src} />
-																	<h3 className="font-bold">{popupInfo.user.name}</h3>
-																</div>
-															</Popup>
-														)}
-													</Map>
-												</div>
-											)}
-										</div>
-									</div>
-									<div className={openTab === 2 ? "block" : "hidden"} id="link2">
-										<Communities />
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+		<div className="flex flex-wrap mt-2">
+			{!session && (
+				<div className="flex flex-row w-full flex-wrap justify-center text-xl font-bold bg-white p-3 bg-opacity-20 rounded-lg">
+					<button
+						type="button"
+						className={`m-10 mx-[20%] ${styles.button}`}
+						onClick={() => signIn("discord", { callbackUrl: "", redirect: true }, { prompt: "none" })}
+					>
+						Log In
+					</button>
 				</div>
-			</div>
-		</>
+			)}
+			{session && (
+				<button
+					type="button"
+					className={`my-5 mx-10 ${styles.button}`}
+					onClick={hasUserLocation ? () => removeLocation() : () => saveLocation()}
+				>
+					{hasUserLocation ? "Remove Location" : "Save Location"}
+				</button>
+			)}
+			{session && (
+				<div className="w-full min-h-[600px]">
+					<Map
+						reuseMaps
+						{...coordinates}
+						initialViewState={{
+							longitude: 139.8,
+							latitude: 35.68,
+							zoom: 2,
+							minZoom: 2,
+							maxZoom: 15,
+						}}
+						onMove={(evt) => setCoordinates(evt.viewState)}
+						mapStyle="mapbox://styles/mapbox/streets-v12"
+					>
+						{!hasUserLocation && (
+							<Marker
+								longitude={coordinates.longitude}
+								latitude={coordinates.latitude}
+								draggable={true}
+								onDragEnd={onMarkerDragEnd}
+							>
+								<img className="max-h-[24px]" src={marker.src} />
+							</Marker>
+						)}
+						{markers}
+						{popupInfo && (
+							<Popup
+								anchor="bottom"
+								longitude={popupInfo.longitude}
+								latitude={popupInfo.latitude}
+								onClose={() => setPopupInfo(null)}
+							>
+								<div className="flex mx-2 mt-2">
+									<img className="max-h-[20px] mr-2" src={discordLogo.src} />
+									<h3 className="font-bold">{popupInfo.user.name}</h3>
+								</div>
+							</Popup>
+						)}
+					</Map>
+				</div>
+			)}
+		</div>
 	)
 }
 
